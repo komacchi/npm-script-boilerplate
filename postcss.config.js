cf568e9a29da7628518ec728bd8c741760ffd9ae
komacchi <komacchi@gmail.com>
2017-07-11 06:24:53 +0900

    add postcss-reporter,postcss-mixins,postcss-extend

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,12 @@
 module.exports = (ctx) => ({
-  map: ctx.options.map,
+  // map: ctx.options.map,
   plugins: [
     require('postcss-import')(),
+    require('postcss-extend')(),
+    require('postcss-mixins')(),
     require('postcss-nested')(),
+    require('postcss-reporter')(),
+    require('postcss-simple-vars')(),
     require('autoprefixer')(),
     require('csswring')()
   ]
