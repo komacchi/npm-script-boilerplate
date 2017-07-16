4f55188676549ffaa5dfd25f13d4930ef3cac66c
komacchi <komacchi@gmail.com>
2017-07-16 17:45:43 +0900

    [wip] stylelint config

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,18 @@
 module.exports = (ctx) => ({
   // map: ctx.options.map,
   plugins: [
-    require('postcss-import')(),
+    require('postcss-import')({
+      plugins: [
+        require('stylelint')
+      ]
+    }),
     require('postcss-extend')(),
     require('postcss-mixins')(),
     require('postcss-nested')(),
     require('postcss-reporter')(),
     require('postcss-simple-vars')(),
     require('postcss-utilities')(),
+    require('postcss-reporter')(),
     require('autoprefixer')(),
     require('csswring')()
   ]
